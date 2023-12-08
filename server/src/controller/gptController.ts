fdac08ea9d201b2dc2be36cae9697fb4d8b55387
Jatin-Ya <jatinyadav20032003@gmail.com>
2023-12-08 20:04:30 +0530

    added query

--- server/src/controller/gptController.ts
+++ server/src/controller/gptController.ts
@@ -4,6 +4,7 @@ import Project from "../models/projectModel";
 import GithubService from "../service/GithubService";
 import { NextFunction } from "connect";
 import { error400, error401, success200 } from "../error/app.error";
+import User from "../models/userModel";
 
 const gptService = new OpenAiService();
 const githubService = new GithubService();
@@ -62,14 +63,25 @@ export const getRoadmap = async (
 }
 
 export const query = async (
-    req: Request<{}, {}, {query : string, path: string}, {}>,
+    req: Request<{}, {}, {query : string, path: string, projectId: string}, {}>,
     res: Response,
     next: NextFunction
 ) => {
-    const { query } = req.body;
+    const { query, path, projectId } = req.body;
 
     try{
-        const response = await gptService.query(query);
+        const project = await Project.findById(projectId).populate("users");
+        const user: any = project?.users[0];
+        const token = user?.githubId?.accessToken;
+        const ownerName = user?.githubId?.name;
+        const repoName = project?.repoDetails?.repoName;
+
+        if (!token) return next(error401("Unauthorized"));
+        if (!ownerName) return next(error401("Unauthorized"));
+        if (!repoName) return next(error401("Unauthorized"));
+
+        const file = await githubService.getFiles(token, ownerName, repoName, [path]);
+        const response = await gptService.query(query, file[path]);
         res.json({ response });
     } catch(err) {
         next(err);
--- server/src/routes/gptRoutes.ts
+++ server/src/routes/gptRoutes.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
 
-import {generateReview, getRoadmap} from "../controller/gptController";
+import {generateReview, getRoadmap, query} from "../controller/gptController";
 import Project from "../models/projectModel";
 
 const router = Router();
 
 router.get("/generateReiew", generateReview);
 
-router.get("/getRoadmap", getRoadmap)
+router.get("/getRoadmap", getRoadmap);
+
+router.post("/query",query);
 
 export default router;
