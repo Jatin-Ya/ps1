a51fba0d7400479bc815e4ab8e1ea050319d6988
Tushar Joshi <tusharjoshi4531@gmail.com>
2023-12-10 12:23:02 +0530

    build fixes

--- server/src/app.ts
+++ server/src/app.ts
@@ -22,7 +22,7 @@ app.use(
 app.use(express.static(path.join(__dirname, "dist")));
 
 app.get("/application", (req, res) => {
-    res.sendFile(path.join(__dirname, "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "public", "index.html"));
 })
 
 app.use("/api/v1/github", githubRoutes);
