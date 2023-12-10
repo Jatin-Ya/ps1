440002fd308dd2bffb36161ba8c82e87c0ba47eb
Tushar Joshi <tusharjoshi4531@gmail.com>
2023-12-10 13:22:05 +0530

    build

--- server/dist/src/routes/authRoutes.js
+++ server/dist/src/routes/authRoutes.js
@@ -117,7 +117,7 @@ router.get("/github/callback", (req, res) =>
                 },
             });
             // access_token=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&scope=repo%2Cuser%2Cwrite%3Aorg&token_type=bearer
-            const access_token = response.data.split("&").split("=")[1];
+            const access_token = response.data.split("&")[0].split("=")[1];
             console.log(access_token);
             console.log(response.data);
             const resp = yield axios_1.default.get(
