1642962c5082febd1199b53781412605a53beb5f
Tushar Joshi <tusharjoshi4531@gmail.com>
2023-12-16 13:03:52 +0530

    CORS

--- client/src/utils/backendFunctions.ts
+++ client/src/utils/backendFunctions.ts
@@ -1,4 +1,5 @@
 export const getBackendBaseUrl = () => {
-    return "http://localhost:8080/api/v1";
+    // return "http://localhost:8080/api/v1";
+    return "https://ps1-test.onrender.com";
     // return "https://ps1-backend.onrender.com/api/v1";
 }
